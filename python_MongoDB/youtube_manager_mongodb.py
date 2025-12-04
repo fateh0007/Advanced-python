9a783fc451a44bf39bf9e760a86be2008cb606f2
fateh0007 <fatehalam123463@gmail.com>
2025-12-04 15:53:01 +0530

    Created yt manger

--- python_MongoDB/youtube_manager_mongodb.py
+++ python_MongoDB/youtube_manager_mongodb.py
@@ -7,6 +7,22 @@ video_collection = db["videos"]
 
 print(video_collection)
 
+def list_videos():
+    for video in video_collection.find():
+        print(f"ID: {video['_id']}, Name: {video['name']}, Time: {video['time']}")
+
+def add_video(name, time): 
+    video_collection.insert_one({"name": name, "time": time})
+
+def update_video(video_id, name, time):
+    video_collection.update_one(
+        {'_id': video_id},
+        {"$set": {'name': name, 'time': time}}
+    )
+
+def delete_video(video_id):
+    video_collection.delete_one({'_id': video_id})
+
 def main():
     while True:
         print("\n YouTube Manager")
@@ -17,6 +33,22 @@ def main():
         print("5. Exit the app")
         choice = input("Enter your choice (1-5): ")
         
-
+        if choice == '1':
+            list_videos()
+        elif choice == '2':
+            name = input("Enter video name: ")
+            time = input("Enter video time: ")
+            add_video(name, time)
+        elif choice == '3':
+            video_id = input("Enter the ID of the video to update: ")
+            name = input("Enter the new name of the video: ")
+            time = input("Enter the new time of the video: ")
+            update_video(video_id,name, time)
+        elif choice == '4':
+            video_id = input("Enter the ID of the video to delete: ")
+            delete_video(video_id)
+        else:
+            break
+            
 if __name__ == "__main__":
     main()
