b1e660b1476eb936f2e7399e796434797ea9df19
fateh0007 <fatehalam123463@gmail.com>
2025-12-04 15:42:17 +0530

    Making list of management choices

--- python_MongoDB/youtube_manager_mongodb.py
+++ python_MongoDB/youtube_manager_mongodb.py
@@ -4,3 +4,19 @@ client = MongoClient("")
 
 db = client["youtube_manager"]
 video_collection = db["videos"]
+
+print(video_collection)
+
+def main():
+    while True:
+        print("\n YouTube Manager")
+        print("1. List all youtube videos")
+        print("2. Add a new youtube video")
+        print("3. Update a youtube video details")
+        print("4. Delete a youtube video")
+        print("5. Exit the app")
+        choice = input("Enter your choice (1-5): ")
+        
+
+if __name__ == "__main__":
+    main()
